948791ec66ced477b3cb8a1eafdc00587d1be707
apsius <38158216+appsius@users.noreply.github.com>
2021-02-14 13:58:53 +0300

    challange-1

--- script.js
+++ script.js
@@ -27,71 +27,78 @@ const countriesContainer = document.querySelector('.countries');
 //    .finally(() => (countriesContainer.style.opacity = 1));
 //};
 
-//const getData = function (data, className = '') {
-//  const html = `<article class="country ${className}">
-//          <img class="country__img" src="${data.flag}" />
-//          <div class="country__data">
-//            <h3 class="country__name">${data.name}</h3>
-//            <h4 class="country__region">${data.region}</h4>
-//            <p class="country__row"><span>${+(
-//              data.population / 1000000
-//            ).toFixed(0)} million</span>POP people</p>
-//            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//            <p class="country__row"><span>💰</span>${
-//              data.currencies[0].name
-//            }</p>
-//          </div>
-//        </article>`;
+const getData = function (data, className = '') {
+  const html = `<article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>${+(
+              data.population / 1000000
+            ).toFixed(0)} million</span>POP people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>`;
 
-//  countriesContainer.insertAdjacentHTML('beforeend', html);
-//};
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
 
-//const renderError = function (msg) {
-//  countriesContainer.insertAdjacentText('beforeend', msg);
-//};
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+};
 
-//const getJSON = function (url, errorMsg = 'Something went wrong!') {
-//  return fetch(url).then(res => {
-//    if (!res.ok) throw new Error(`${errorMsg} ${res.status}`);
-//    return res.json();
-//  });
-//};
+const getJSON = function (url, errorMsg = 'Something went wrong!') {
+  return fetch(url).then(res => {
+    if (!res.ok) throw new Error(`${errorMsg} ${res.status}`);
+    return res.json();
+  });
+};
 
-//const getCountryAndNeighbour = function (country) {
-//  getJSON(
-//    `https://restcountries.eu/rest/v2/name/${country}`,
-//    'Country not found!'
-//  )
-//    .then(data => {
-//      getData(data[0]);
+const getCountryAndNeighbour = function (lat, lng) {
+  getJSON(
+    `https://geocode.xyz/${lat},${lng}?geoit=json`,
+    'Only 3 requests are allowed per second!'
+  )
+    .then(data => {
+      if (!data.city || !data.country) throw new Error('No coordinates found!');
+      const { country } = data;
 
-//      const neighbour = data[0].borders[0];
-//      if (!neighbour) throw new Error('No neighbour found');
+      return getJSON(
+        `https://restcountries.eu/rest/v2/name/${country}`,
+        'Country not found!'
+      );
+    })
+    .then(data1 => {
+      getData(data1[0]);
 
-//      return getJSON(
-//        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
-//        'No country found!'
-//      );
-//    })
-//    .then(data2 => {
-//      getData(data2, 'neighbour');
-//    })
-//    .catch(err => {
-//      console.error(err);
-//      renderError(`Error: ${err.message} 💥💥💥`);
-//    })
-//    .finally(() => {
-//      countriesContainer.style.opacity = 1;
-//    });
-//};
+      const neighbour = data1[0].borders[0];
+      if (!neighbour) throw new Error('No neighbour found');
 
-//btn.addEventListener('click', function () {
-//  getCountryAndNeighbour('tunisia');
+      return getJSON(
+        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+        'No country found!'
+      );
+    })
+    .then(data2 => getData(data2, 'neighbour'))
+    .catch(err => {
+      console.error(err);
+      renderError(`Error: ${err.message} 💥💥💥`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  //getCountryAndNeighbour(19.1852115, 35.3631897);
 });
 
-getCountryAndNeighbour('australia');
+getCountryAndNeighbour(18.2869546, 42.6773798);
 
-// ********************************* 2 *********************************
+// NOTE ********************************* 2 *********************************
 //const getCountryAndNeighbour = function (country) {
 //  const request = new XMLHttpRequest();
 //  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
@@ -149,6 +156,48 @@ getCountryAndNeighbour('australia');
 //  });
 //};
 
+//const getCountryAndNeighbour = function (lat, lng) {
+//  getJSON(
+//    `https://geocode.xyz/${lat},${lng}?geoit=json`,
+//    'Only 3 requests are allowed per seconds!'
+//  )
+//    .then(data => {
+//      console.log(data);
+//      if (data.error) throw new Error('No coordinates found!');
+//      console.log(`You are in ${data.city}, ${data.country}`);
+
+//      const { country } = data;
+
+//      return getJSON(
+//        `https://restcountries.eu/rest/v2/name/${country}`,
+//        'No country found!'
+//      );
+//    })
+//    .then(data => {
+//      //console.log(data);
+//      getCountry(data[0]);
+
+//      const neighbour = data[0].borders[0];
+//      if (!neighbour) throw new Error('No neighbour found!');
+
+//      return getJSON(
+//        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+//        'No country found!'
+//      );
+//    })
+//    .then(data2 => getCountry(data2, 'neighbour'))
+//    .catch(err => {
+//      renderError(`Error: ${err.message} 💥💥💥 Please try again later!`);
+//    })
+//    .finally(() => (countriesContainer.style.opacity = 1));
+//};
+
+//btn.addEventListener('click', function () {
+//getCountryAndNeighbour(-21.1600173, 95.3624096);
+//});
+
+//getCountryAndNeighbour(-18.8791902, 47.5079055);
+
 //const getCountryAndNeighbour = function (country) {
 //  getJSON(
 //    `https://restcountries.eu/rest/v2/name/${country}`,
@@ -172,11 +221,7 @@ getCountryAndNeighbour('australia');
 //    .finally(() => (countriesContainer.style.opacity = 1));
 //};
 
-//btn.addEventListener('click', function () {
-//  getCountryAndNeighbour('australia');
-//});
-
-// ********************************* 3 *********************************
+// NOTE ********************************* 3 *********************************
 //const getData = function (country) {
 //  const request = new XMLHttpRequest();
 //  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
@@ -232,15 +277,24 @@ getCountryAndNeighbour('australia');
 //  });
 //};
 
-//const getData = function (country) {
+//const getData = function (lat, lng) {
 //  getJSON(
-//    `https://restcountries.eu/rest/v2/name/${country}`,
-//    'Country not found'
+//    `https://geocode.xyz/${lat},${lng}?geoit=json`,
+//    'Only 3 requests are alllowed per second!'
 //  )
 //    .then(data => {
-//      console.log(getCountry(data[0]));
+//      if (data.error) throw new Error('Coordinates are not true!');
+//      console.log(`You are in ${data.city}, ${data.country}`);
+//      const { country } = data;
 
-//      const neighbour = data[0].borders[0];
+//      return getJSON(
+//        `https://restcountries.eu/rest/v2/name/${country}`,
+//        'Country not found'
+//      );
+//    })
+//    .then(res => {
+//      getCountry(res[0]);
+//      const neighbour = res[0].borders[0];
 //      if (!neighbour) throw new Error('No neighbour found!');
 
 //      return getJSON(
@@ -248,7 +302,9 @@ getCountryAndNeighbour('australia');
 //        'Country not found'
 //      );
 //    })
-//    .then(data2 => getCountry(data2, 'neighbour'))
+//    .then(data2 => {
+//      getCountry(data2, 'neighbour');
+//    })
 //    .catch(err => {
 //      console.log(`Something went wrong, ${err}`);
 //      renderError(
@@ -261,7 +317,66 @@ getCountryAndNeighbour('australia');
 //};
 
 //btn.addEventListener('click', function () {
-//  getData('turkey');
+//  getData(34.828058, 35.828058);
 //});
 
-//getData('australia');
+//getData(35.828058, 35.828058);
+
+//******************************************************************
+//*** Coding Challenge #1
+//******************************* 1 ********************************
+//const whereIAm = function (lat, lng) {
+//  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//    .then(res => {
+//      if (!res.ok)
+//        throw new Error(
+//          `Only 3 requests are alllowed per second! (${res.status})`
+//        );
+//      return res.json();
+//    })
+//    .then(data => {
+//      if (data.error) throw new Error('Coordinates are not true!');
+//      console.log(`You are in ${data.city}, ${data.country}`);
+//    })
+//    .catch(err => {
+//      console.log(`${err.message}`);
+//    });
+//};
+
+//whereIAm(37.91898, 40.1966645);
+
+//******************************* 2 ********************************
+//const whereIAm = function (lat, lng) {
+//  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//    .then(res => {
+//      if (!res.ok) throw new Error('Only 3 requests are allowed per seconds!');
+//      return res.json();
+//    })
+//    .then(data => {
+//      if (data.error) throw new Error('No coordinates found!');
+//      console.log(`You are in ${data.city}, ${data.country}`);
+//    })
+//    .catch(err => {
+//      console.error(err.message);
+//    });
+//};
+
+////whereIAm(29929292992929, 43.1640004);
+//whereIAm(36.3566484, 43.1640004);
+
+//******************************* 3 ********************************
+//const whereIAm = function (lat, lng) {
+//  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//    .then(res => {
+//      return res.json();
+//    })
+//    .then(data => {
+//      if (data.error) throw new Error('No coordinates found!');
+//      console.log(data);
+//    })
+//    .catch(err => {
+//      console.log(err.message);
+//    });
+//};
+
+//whereIAm(9.0585977, 38.7241055);
